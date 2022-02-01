3f335327952dd7226980547261d606abf4c411df
heba2000 <hebamamdouh15@yahoo.com>
2022-02-01 16:18:02 +0200

    "order controller & order Route files created"

--- server/routes/store routes/storeMedicineRoute.js
+++ server/routes/store routes/storeMedicineRoute.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Medicine = require("../../models/medicine");
+
 const {
   create,
   update,
---
+++ server/routes/store routes/storeOrdersRoute.js
@@ -0,0 +1,4 @@
+const express = require("express");
+const router = express.Router();
+const Orders = require("../../models/order");
+
