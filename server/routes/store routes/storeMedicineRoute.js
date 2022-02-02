b7cea73dc1b6966dafaa296c2330feeb836b0fb3
sarahsaleh082 <sarahsaleh082@gmail.com>
2022-02-01 23:08:49 -0800

    add image middleware

--- server/routes/store routes/storeMedicineRoute.js
+++ server/routes/store routes/storeMedicineRoute.js
@@ -5,23 +5,41 @@ const {
   create,
   update,
   updateQuantity,
+  deleteOne,
+  getById
 } = require("../../controllers/store controllers/storeMedController");
 
-router.post("/medicine/add", async (req, res, next) => {
-  const body = req.body;
-  body.categories = body.categories.split(" ");
-  const medArr = await Medicine.find({});
-  body.id = medArr.length + 1;
-  create(body)
+const uploadS3 = require("../../middlewares/imageMiddleware");
+
+router.post(
+  "/medicine/add",
+  uploadS3.single("image"),
+  async (req, res, next) => {
+    const body = req.body;
+    body.image = req.file?.location;
+    body.categories = body.categories.split(" ");
+    const medArr = await Medicine.find({});
+    body.id = medArr.length + 1;
+    create(body)
+      .then((doc) => res.json(doc))
+      .catch((e) => next(e));
+  }
+);
+
+router.patch("/medicine/:id", uploadS3.single("image"), (req, res, next) => {
+  const medId = req.params.id;
+  const medicine = req.body;
+  medicine.image = req.file?.location;
+  update(medId, medicine)
     .then((doc) => res.json(doc))
     .catch((e) => next(e));
 });
 
-router.patch("/medicine/:id", (req, res, next) => {
+router.delete("/medicine/delete/:id", (req, res, next) => {
   const medId = req.params.id;
   const medicine = req.body;
 
-  update(medId, medicine)
+  deleteOne(medId, medicine)
     .then((doc) => res.json(doc))
     .catch((e) => next(e));
 });
@@ -30,6 +48,14 @@ router.patch("/medicine/quantity/:id", (req, res, next) => {
   const medId = req.params.id;
   const medicineQuantity = req.body.quantity;
   updateQuantity(medId, medicineQuantity)
+    .then((doc) => res.json(doc))
+    .catch((e) => next(e));
+});
+
+router.get("/medicine/details/:id", (req, res, next) => {
+  const medId = req.params.id;
+  console.log(medId);
+  getById(medId)
     .then((doc) => res.json(doc))
     .catch((e) => next(e));
 });
