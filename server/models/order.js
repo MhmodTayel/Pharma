3a9548c49590f2b76c8e2b555816c76968c48881
ahmedSam22 <58396761+ahmedSam22@users.noreply.github.com>
2022-02-03 17:40:22 +0200

    update order model

--- server/models/order.js
+++ server/models/order.js
@@ -6,6 +6,8 @@ const orderSchema = new mongoose.Schema({
         default: 1,
         required: true
     },
+    client: { _id: { type: mongoose.Schema.Types.ObjectId, ref: 'Pharmacist' } },
+
     number: {
         type: String,
         required: true,
@@ -21,7 +23,7 @@ const orderSchema = new mongoose.Schema({
     discount: Boolean,
     medicines: [
         {
-            name: String,
+            _id: { type: mongoose.Schema.Types.ObjectId, ref: 'Medicine' },
             quantity: Number
         },
     ],
