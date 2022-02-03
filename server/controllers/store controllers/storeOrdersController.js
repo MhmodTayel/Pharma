532316d3f3282ae14afa039b7d4cf221457f9b12
heba2000 <hebamamdouh15@yahoo.com>
2022-02-03 20:01:24 +0200

    "update"

--- server/controllers/store controllers/storeOrdersController.js
+++ server/controllers/store controllers/storeOrdersController.js
@@ -1,9 +1,10 @@
 const Orders = require("../../models/order");
 
+const createOrder = (order) => Orders.create(order);
 const getOrderById = (id) => Orders.findOne({ id: id })
 const getAllOrders = () => Orders.find();
 
 
-module.exports = {getOrderById};
+module.exports = {getOrderById , createOrder , getAllOrders};
 
 
--- server/routes/store routes/storeOrdersRoute.js
+++ server/routes/store routes/storeOrdersRoute.js
@@ -3,21 +3,29 @@ const router = express.Router();
 const Orders = require("../../models/order");
 const {
     getOrderById,
-    getAllOrders
+    getAllOrders,
+    createOrder
 } = require("./../../controllers/store controllers/storeOrdersController")
 
+router.post("/orders/add", (req, res, next) => {
+    const order = req.body;
+    console.log(order)
+    // createOrder(order)
+    //   .then((doc) => res.json(doc))
+    //   .catch((e) => next(e));
+  });
 
-router.get("/orders/all", (req, res, next) => {
-        getAllOrders()
-        .then((doc) => res.json(doc))
-        .catch((e) => next(e));
-});
+// router.get("/orders/all", (req, res, next) => {
+//         getAllOrders()
+//         .then((doc) => res.json(doc))
+//         .catch((e) => next(e));
+// });
 
-router.get("/orders/orderDetails/:id", (req, res, next) => {
-    const orderID = req.params.id;
-    getOrderById(orderID)
-        .then((doc) => res.json(doc))
-        .catch((e) => next(e));
-});
+// router.get("/orders/orderDetails/:id", (req, res, next) => {
+//     const orderID = req.params.id;
+//     getOrderById(orderID)
+//         .then((doc) => res.json(doc))
+//         .catch((e) => next(e));
+// });
 
 module.exports = router;
