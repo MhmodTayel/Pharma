bd2eeb6c4c849a582bf16abfd41b824989b1defa
ElhamHeshamElsergany <elhamhesham18@gmail.com>
2022-02-05 14:59:10 +0200

    add get all user orders by user id

--- server/controllers/store controllers/storeOrdersController.js
+++ server/controllers/store controllers/storeOrdersController.js
@@ -1,10 +1,12 @@
 const Orders = require("../../models/order");
 
 const createOrder = (order) => Orders.create(order);
-const getOrderById = (id) => Orders.findOne({ id: id }).populate({path:'medicines.id' , select:'name quantity pharmPrice storePrice -_id'})
-const getAllOrders = () => Orders.find().populate({path:'medicines.id' , select:'name quantity pharmPrice storePrice -_id'});
+const getOrderById = (id) => Orders.findOne({ id: id }).populate({ path: 'medicines.id', select: 'name quantity pharmPrice storePrice -_id' })
+const getAllOrders = () => Orders.find().populate({ path: 'medicines.id', select: 'name quantity pharmPrice storePrice -_id' });
 
+// get all orders by user id 
+const getOrdersByUserId = (id) => Orders.find({ client: _id }).populate("client");
 
-module.exports = {getOrderById , createOrder , getAllOrders};
+module.exports = { getOrderById, createOrder, getAllOrders , getOrdersByUserId };
 
 
