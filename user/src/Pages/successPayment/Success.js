4c05dd658b07638c8ec951ae99272f9b334102de
Mahmoud Tayel <mhmodtayel0050@gmail.com>
2022-02-21 20:36:59 +0200

    success page style

--- user/src/Pages/successPayment/Success.js
+++ user/src/Pages/successPayment/Success.js
@@ -1,14 +1,22 @@
 import React, { useEffect, useState } from "react";
-import { createOrder, getOrders,reduceMedQuantity } from "../../services/userService";
+import {
+  createOrder,
+  getOrders,
+  reduceMedQuantity,
+} from "../../services/userService";
+import styles from "./Success.module.scss";
 
 export default function Success() {
   const [response, setResponse] = useState({});
+  const token = localStorage.getItem("token") || null;
+  let loggedUser = null;
+  if (token) loggedUser = JSON.parse(atob(token.split(".")[1]));
+
   useEffect(() => {
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
     const sessionId = urlParams.get("session_id");
     getOrders(sessionId).then((res) => {
-      console.log(res)
       setResponse(res.data);
       const lineItems = res.data.lineItems.data.map((item) => {
         return {
@@ -24,32 +32,56 @@ export default function Success() {
         created: totalCharge.created,
         receipt_url: totalCharge.receipt_url,
       };
-      const token = localStorage.getItem("token") || null;
-      let loggedUserId = null
-      if (token)
-        loggedUserId = JSON.parse(atob(token.split(".")[1]))._id ;
+
       const order = {
         cost: res.data.paymentIntent.amount,
         ...charge,
         medicines: lineItems,
-        client: loggedUserId,
+        client: loggedUser._id,
         inProgress: true,
         numberOfCat: lineItems.length,
         fulfilled: false,
         discount: false,
       };
-      createOrder(order).then((res)=> {
-        console.log(res)
-      })
+      createOrder(order).then((res) => {});
 
-      const meds = JSON.parse(res.data.session.metadata.data)
-      meds.forEach(medicine=> {
-        reduceMedQuantity(medicine).then(res=> {
-          console.log(res)
-        })
-      })
+      const meds = JSON.parse(res.data.session.metadata.data);
+      meds.forEach((medicine) => {
+        reduceMedQuantity(medicine).then((res) => {});
+      });
     });
   }, []);
 
-  return <div>Success</div>;
+  return (
+    <>
+      <div className={styles.container}>
+        <div className={styles.card}>
+          {/* <img src={require("../../Assets/Images/accept.png")} alt="" /> */}
+          <div className={styles.icon}>
+          <div className={styles.success_icon}>
+            <div className={styles.success_icon__tip}></div>
+            <div className={styles.success_icon__long}></div>
+          </div>
+          </div>
+          <h5>Thanks {loggedUser.username}, for your order</h5>
+          <p>
+            You have just completed the payment, The order will reach out to you
+            as soon as possible
+          </p>
+          <div className={styles.action}>
+            <a target="_blank" className={styles.btnOrders}>
+              View my orders
+            </a>
+            <a
+              target="_blank"
+              href={response?.paymentIntent?.charges?.data[0].receipt_url}
+              className={styles.btnReceipt}
+            >
+              View receipt
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
