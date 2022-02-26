b2012133079668b46ab82752b3cf0447a1e9464c
Mahmoud Tayel <mhmodtayel0050@gmail.com>
2022-02-27 00:25:28 +0200

    fix api calling

--- user/src/Pages/singleCategory/Single-category.js
+++ user/src/Pages/singleCategory/Single-category.js
@@ -1,38 +1,34 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
-import { getMedByCat } from '../../services/userService';
-
+import { getMedByCat } from "../../services/userService";
 
 export default function SingleCategory() {
-    const [setDetails] = useState({});
-    const { cat } = useParams();
+  const [details,setDetails] = useState({});
+  const { cat } = useParams();
 
-    const back = () =>{
-        window.history.back()
-    }
+  const back = () => {
+    window.history.back();
+  };
 
-    console.log(cat);
-    
+  React.useEffect(() => {
+    getMedByCat("test")
+      .then((res) => {
+        setDetails(res.data);
+        console.log(res);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
-    React.useEffect(()=>{
-        getMedByCat("test")
-        .then((res) => {setDetails(res.data)
-        console.log(res,'test for response direct');})
-        .catch((err) => console.log(err));
-        console.log(setDetails , 'test for setDetails');
-}, []);
+  return (
+    <div>
+      <button className="back btn btn-outline-danger" onClick={() => back()}>
+        {" "}
+        back
+      </button>
 
-
-    return (
-        <div>
-
-                <button className="back btn btn-outline-danger" onClick={() => back()}> back</button>
-
-            <h1>test test test </h1>
-            <hr />
-
-
-        </div>
-    );
+      <h1>test test test </h1>
+      <hr />
+    </div>
+  );
 }
